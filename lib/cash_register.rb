262a1b29d0a2ba557268ecc2fc0c66fcee01884c
sbalayan1 <balayans2014@yahoo.com>
2022-10-13 03:44:14 -0700

    commit tree d06e1f53ec07b697a211dc1e4133ff1732419ddb

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,11 +6,10 @@ class CashRegister
         @total = 0
         @discount = discount
         @items = []
-        @last_transaction = 0
     end
 
     def add_item title, price, quantity = 1
-        self.last_transaction = price * quantity
+        self.last_transaction = price * quantity #refers to the last_transaction setter method
         self.total += price * quantity
         quantity.times do
             items << title
@@ -27,7 +26,15 @@ class CashRegister
     end
 
     def void_last_transaction
-        self.total -= last_transaction
+        self.total -= last_transaction #note upon instanstiation, last_transaction is nil because the variable is not set to any value upon initialization though attr_accessor creates its instance methods. 
     end
 end
+
+# register = CashRegister.new(20)
+# register.void_last_transaction
+
+# # register.add_item('banana', 50, 5) #total = 250, last_transaction = 250
+# # register.void_last_transaction #total = 0
+# # register.void_last_transaction
+# puts register.total
 
